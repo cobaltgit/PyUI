6abed515a4f359cd9bc96dd14052608eb1621c2b
Christopher Jacobs <chrisj951@gmail.com>
2025-04-30 08:33:40 -0700

    Get the list of systems dynamically based on active configs

--- main-ui/mainui.py
+++ main-ui/mainui.py
@@ -2,23 +2,22 @@ import sdl2
 import sdl2.ext
 from controller.controller import Controller
 from screen import Screen
+from games.utils.game_system_utils import GameSystemUtils
 from views.listview import ListView
-from pathlib import Path
 from themes.any_theme import AnyTheme
 
 sdl2.ext.init(controller=True)
 
 theme = AnyTheme("/mnt/sdcard/Themes/SPRUCE/") 
 controller = Controller()
-screen = Screen(theme);
+screen = Screen(theme)
+game_utils = GameSystemUtils()
 
 def run_option_toggle_ui():
-    roms_folders = [f.name for f in Path("/mnt/sdcard/Roms/").iterdir() if f.is_dir()]
-    roms_folders.sort()
-    options_list = ListView(screen,controller,roms_folders);
+    options_list = ListView(screen,controller,game_utils.get_active_systems())
     selected = options_list.get_selection()
     print(f"{selected} was selected")
 
-run_option_toggle_ui();
+run_option_toggle_ui()
     
 
