ffcbd6f8e7f42054a8065901606a4963fd865c16
Christopher Jacobs <chrisj951@gmail.com>
2025-05-06 16:23:50 -0700

    Slightly speed up rom dir reading

--- main-ui/games/utils/rom_utils.py
+++ main-ui/games/utils/rom_utils.py
@@ -8,26 +8,11 @@ class RomUtils:
         return self.roms_path
     
     def get_roms(self, system):
-        # Construct the full directory path
         directory = os.path.join(self.roms_path, system)
-        
-        # List to store valid files
-        valid_files = []
-        
-        # Iterate through the directory
-        for filename in os.listdir(directory):
-            filepath = os.path.join(directory, filename)
-            
-            # Skip folders and excluded file types or filenames starting with '.'
-            if os.path.isdir(filepath):
-                continue
-            if filename.endswith(('.xml', '.txt', '.db')) or filename.startswith('.'):
-                continue
-            
-            # Add valid file to the list
-            valid_files.append(filepath)
-            print(f"Added {filepath}")
-        
-        valid_files.sort()
-        
+        valid_files = sorted(
+            entry.path for entry in os.scandir(directory)
+            if entry.is_file(follow_symlinks=False)
+            and not entry.name.startswith('.')
+            and not entry.name.endswith(('.xml', '.txt', '.db'))
+        )
         return valid_files
