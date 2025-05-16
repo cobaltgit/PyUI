7c78dc2424c37da0f1b75958c1714a9c829800a8
Christopher Jacobs <chrisj951@gmail.com>
2025-05-16 10:24:26 -0700

    Adjust game system select menu to be compatible with stock and themes

--- main-ui/menus/games/game_system_select_menu.py
+++ main-ui/menus/games/game_system_select_menu.py
@@ -29,32 +29,18 @@ class GameSystemSelectMenu:
         self.use_emu_cfg = False
         self.view_creator = ViewCreator(display,controller,device,theme)
         self.game_system_select_menu_popup = GameSystemSelectMenuPopup(display,controller,device,theme)
+        self.common_icon_mappings = {
+            "PPSSPP": "psp",
+            "FFPLAY":"ffplay",
+            "MPV":"ffplay",
+            "WSC":"ws",
+            "FAKE8":"pico",
+            "PICO8":"pico",
+            "THIRTYTWOX":"32X"
+        }
 
     def get_system_name_for_icon(self, sys_config):        
         return os.path.splitext(os.path.basename(sys_config.get_icon()))[0]
-
-    def choose_existing_file(self, primary_path, fallback_path, fallback_dir):
-
-        try:
-            if os.path.exists(primary_path):
-                return primary_path
-        except Exception:
-            pass
-
-        try:
-            if os.path.exists(fallback_path):
-                return fallback_path
-        except Exception:
-            pass
-                    
-        fallback_full_path = os.path.join(fallback_dir,fallback_path)
-        try:
-            if os.path.exists(fallback_full_path):
-                return fallback_full_path
-        except Exception:
-            pass
-
-        return None
     
     def get_first_existing_path(self,icon_system_name_priority):
         for path in icon_system_name_priority:
@@ -67,21 +53,29 @@ class GameSystemSelectMenu:
 
     def get_images(self, game_system : GameSystem):
         icon_system_name = self.get_system_name_for_icon(game_system.game_system_config)
-        icon_system_name_priority = [self.theme.get_system_icon(icon_system_name),
-                                     self.theme.get_system_icon(game_system.folder_name.lower()),
-                                     self.theme.get_system_icon(game_system.display_name.lower()),
-                                     game_system.game_system_config.get_icon()]
+        icon_system_name_priority = []
+        icon_system_name_priority.append(self.theme.get_system_icon(icon_system_name))
+        icon_system_name_priority.append(self.theme.get_system_icon(game_system.folder_name.lower()))
+        icon_system_name_priority.append(self.theme.get_system_icon(game_system.display_name.lower()))
+        if game_system.folder_name in self.common_icon_mappings:
+            icon_system_name_priority.append(self.theme.get_system_icon(self.common_icon_mappings[game_system.folder_name]))
+        icon_system_name_priority.append(game_system.game_system_config.get_icon())
+
         if(game_system.game_system_config.get_icon() is not None):
             icon_system_name_priority.append(os.path.join(game_system.game_system_config.get_emu_folder(),game_system.game_system_config.get_icon()))
 
-        selected_icon_system_name_priority = [self.theme.get_system_icon_selected(icon_system_name),
-                                                self.theme.get_system_icon_selected(game_system.folder_name.lower()),
-                                                self.theme.get_system_icon_selected(game_system.display_name.lower()),
-                                                game_system.game_system_config.get_icon_selected()]
+        selected_icon_system_name_priority = []
+        selected_icon_system_name_priority.append(self.theme.get_system_icon_selected(icon_system_name))
+        selected_icon_system_name_priority.append(self.theme.get_system_icon_selected(game_system.folder_name.lower()))
+        selected_icon_system_name_priority.append(self.theme.get_system_icon_selected(game_system.display_name.lower()))
+        if game_system.folder_name in self.common_icon_mappings:
+            selected_icon_system_name_priority.append(self.theme.get_system_icon_selected(self.common_icon_mappings[game_system.folder_name]))
+        selected_icon_system_name_priority.append(game_system.game_system_config.get_icon_selected())
         
         if(game_system.game_system_config.get_icon_selected() is not None):
             icon_system_name_priority.append(os.path.join(game_system.game_system_config.get_emu_folder(),game_system.game_system_config.get_icon_selected()))
 
+        
         
         return self.get_first_existing_path(icon_system_name_priority), self.get_first_existing_path(selected_icon_system_name_priority),
     
@@ -93,6 +87,7 @@ class GameSystemSelectMenu:
             sys_config = game_system.game_system_config
             print(f"{sys_config}")
             image_path, image_path_selected = self.get_images(game_system)
+            print(f"{game_system.display_name} using {image_path}")
             icon = image_path_selected
             systems_list.append(
                 GridOrListEntry(
