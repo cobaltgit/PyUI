dcb8f9b71569dd282b616cf142996a1f7a2527e0
Christopher Jacobs <chrisj951@gmail.com>
2025-05-16 19:07:15 -0700

    Fix optionSelectUi w/ latest changes

--- main-ui/OptionSelectUI.py
+++ main-ui/OptionSelectUI.py
@@ -3,11 +3,10 @@ import os
 import subprocess
 import sys
 from controller.controller_inputs import ControllerInput
-from display.render_mode import RenderMode
+from devices.device import Device
 import sdl2
 import sdl2.ext
 
-from menus.main_menu import MainMenu
 from controller.controller import Controller
 from display.display import Display
 from themes.theme import Theme
@@ -15,7 +14,6 @@ from devices.miyoo_flip import MiyooFlip
 from utils.logger import PyUiLogger
 from utils.py_ui_config import PyUiConfig
 from views.grid_or_list_entry import GridOrListEntry
-from views.image_list_view import ImageListView
 from views.selection import Selection
 from views.view_creator import ViewCreator
 from views.view_type import ViewType
@@ -29,12 +27,11 @@ config.load()
 
 selected_theme = os.path.join(config["themeDir"],config["theme"])
                               
-theme = Theme(os.path.join(config["themeDir"],config["theme"]))
+Theme.init(os.path.join(config["themeDir"],config["theme"]))
 
-device = MiyooFlip()
-display = Display(theme, device)
-controller = Controller(device, config)
-view_creator = ViewCreator(display,controller,device,theme)
+Device.init(MiyooFlip())
+Display.init()
+Controller.init(config)
 
 title = sys.argv[1]
 input_json = sys.argv[2]
@@ -58,7 +55,7 @@ for entry in data:
         )
     )
 
-view = view_creator.create_view(
+view = ViewCreator.create_view(
         view_type=ViewType.TEXT_AND_IMAGE_LIST_VIEW,
         top_bar_text=title,
         options=option_list, 
